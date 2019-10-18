8fc70727aec6109731a2781f5353ef32d0600f97
zakve <zaklas.martin@gmail.com>
2019-10-18 11:57:11 +0200

    Add navigate btn

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from "react-native";
 
+import { Button } from 'react-native-elements';
+
 const CategoriesScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Categories Screen!</Text>
+            <Button
+                title="Go to Meals" onPress={() => {
+                    props.navigation.navigate('CategoryMeals')
+                }}
+                type="outline"
+            />
         </View>
     )
 }
