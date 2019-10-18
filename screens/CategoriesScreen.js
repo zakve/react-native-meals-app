c2eb676eecac07f31d20e9c8c4520a426cf6e3dd
zakve <zaklas.martin@gmail.com>
2019-10-18 10:47:07 +0200

    Add Screens

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+const CategoriesScreen = props => {
+    <View style={styles.screen}>
+        <Text>The Categories Screen!</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default CategoriesScreen;
---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+const CategoryMealsScreen = props => {
+    <View style={styles.screen}>
+        <Text>The Category meals Screen!</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default CategoryMealsScreen;
