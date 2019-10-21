3095acb60ce0ccbc96bf7afd75992711819f8392
zakve <zaklas.martin@gmail.com>
2019-10-21 10:20:03 +0200

    Add Card styles for android

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, View, Text, StyleSheet, Platform, TouchableNativeFeedback } from "react-native";
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 
 const CategoryGridTile = props => {
+    let TouchableCmp = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableCmp = TouchableNativeFeedback;
+    }
+
     return (
-        <TouchableOpacity
-            style={styles.gridItem}
-            onPress={props.onSelect}
-        >
-            <Card
-                containerStyle={{ ...styles.card, ...{ backgroundColor: props.bgColor } }}
-                dividerStyle={styles.divider}
-                title={props.title}>
-            </Card>
-        </TouchableOpacity>
+        <View style={styles.gridItem}>
+            <TouchableCmp
+                style={{ flex: 1 }}
+                onPress={props.onSelect}
+            >
+                <Card
+                    containerStyle={{ ...styles.card, ...{ backgroundColor: props.bgColor } }}
+                    dividerStyle={styles.divider}
+                    title={props.title}>
+                </Card>
+            </TouchableCmp>
+        </View>
+
     )
 }
 
 const styles = StyleSheet.create({
     gridItem: {
-        flex: 1
+        flex: 1,
+        borderRadius: 10,
+        overflow: 'hidden'
     },
     card: {
         height: 150,
