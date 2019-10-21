c1dae113ace3e63c38c700cc89e18e802708b91f
zakve <zaklas.martin@gmail.com>
2019-10-21 13:45:50 +0200

    Add react-native-vector-icons

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -6,21 +6,22 @@ import { ListItem } from 'react-native-elements';
 import { CATEGORIES, MEALS } from '../data/dummy-data'
 
 const CategoryMealsScreen = props => {
-    const renderMealItem = itemData => {
-        return (<View><Text>{itemData.item.title}</Text></View>)
-    }
-
     const catId = props.navigation.getParam('categoryId');
 
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
     return (
-        <View style={styles.screen}>
-            <FlatList
-                data={displayedMeals}
-                keyExtractor={(item, index) => item.id}
-                renderItem={renderMealItem}
-            />
+        <View>
+            {
+                displayedMeals.map((l, i) => (
+                    <ListItem
+                        key={i}
+                        title={l.title}
+                        bottomDivider
+                        chevron
+                    />
+                ))
+            }
         </View>
     )
 }
